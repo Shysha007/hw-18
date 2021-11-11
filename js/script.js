b2ce153b1cc6657d270b9281a21151c2149c4071
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-11-11 02:17:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //task 1
-const arr1 =  [1, 2, 3];
-const arr2 = [4, 5, 6];
-const arr3 = arr1.concat(arr2);
+let arr1 =  [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = arr1.concat(arr2);
 console.log(arr3);
 
 // task 2
